525a891e9b72724aa9e16f691c9c98e91afb8552
Amro <amroamroamro@gmail.com>
2016-06-01 21:05:58 +0300

    cleanup switch lang script

--- src/_switch_lang.js
+++ src/_switch_lang.js
@@ -1,95 +1,7 @@
-// REFERENCE: http://userscripts-mirror.org/scripts/show/71052
-// REFERENCE: http://davidwalsh.name/twitter-dropdown-jquery
-function add_language_selection_menu() {
-    "use strict";
-
-    // we require jQuery to be already loaded in the page
-    if (typeof jQuery == 'undefined') { return; }
-
-    // languages CSS classes
-    var languages = ['default', 'lang-html', 'lang-c', 'lang-java', 'lang-cs',
-        'lang-sh', 'lang-pl', 'lang-py', 'lang-rb', 'lang-js', 'lang-matlab'];
-
-    // return closure
-    var makeClickHandler = function (pre, lang) {
-        // create closure
-        return function (e) {
-            // remove existing formatting inside <code> tag, by setting content to plain text again
-            unprettify(pre.children('code'));
-
-            // set new prettify class
-            pre.removeClass();
-            pre.addClass('prettyprint ' + lang);
-
-            // change language indicated
-            $(this).parent().prev('.pp-lang-link').children('span').text(lang);
-
-            // hide languge menu
-            //$(this).parent().slideToggle();
-
-            // re-apply syntax highlighting
-            prettyPrint();
-
-            // stop default link-clicking behaviour
-            e.preventDefault();
-        };
-    };
-
-    // go through each <pre> block, and add language selection menu
-    $('pre.prettyprint').each(function () {
-        // <pre> block
-        var code = $(this);
-
-        // current language used
-        var currLang = $.trim(this.className.replace(/prettyprint(ed)?/g, ''));
-        if (!currLang) { currLang = 'default'; }
-
-        // create <div> of language selector button
-        var button = $('<div class="pp-lang-button" title="choose language"></div>');
-
-        // create and add toggle link
-        var link = $('<a class="pp-lang-link"><span>' + currLang + '</span></a>').appendTo(button);
-
-        // create dropmenu and add to button
-        var menu = $('<div class="pp-lang-menu"></div>').appendTo(button);
-
-        // transparency animation on hover
-        button.hover(
-            function () { $(this).animate({opacity: 1.0}, 'fast'); },
-            function () { $(this).animate({opacity: 0.7}); }
-        );
-
-        // button is click event
-        link.click(function (e) {
-            // set button as active/non-active
-            $(this).toggleClass('pp-link-active');
-
-            // show/hide menu
-            $(this).next('.pp-lang-menu').slideToggle();
-
-            // stop default link-clicking behaviour
-            e.preventDefault();
-        });
-
-        // populate it with entries for every language
-        for (var i = 0; i < languages.length; i++) {
-            // create link, hook up the click event, and add it to menu
-            $('<a title="set language to: ' + languages[i] + '">' + languages[i] + '</a>')
-                .css({'cursor': 'pointer', 'font-size': 'small'})
-                .click( makeClickHandler(code, languages[i]) ).appendTo(menu);
-        }
-
-        // add button to DOM just before the <pre> block
-        button.insertBefore(code);
-    });
-}
-
-function unprettify(codeNode) {
-    // Note: el.innerHTML, el.textContent vs. $(el).html(), $(el).text()
-    var code = $(codeNode);
+function unprettify(code) {
     // html encoded
     var encodedStr = code.html()
-        .replace(/<br[^>]*>/g, "\n")
+        .replace(/<br[^>]*>/g, '\n')
         .replace(/&nbsp;/g, ' ');
     // decode html entities
     var decodedStr = $('<div/>').html(encodedStr).text();
@@ -97,15 +9,97 @@ function unprettify(codeNode) {
     code.text(decodedStr);
 }
 
+// REFERENCE: http://userscripts-mirror.org/scripts/show/71052
+// REFERENCE: http://davidwalsh.name/twitter-dropdown-jquery
 /*
 <div class="pp-lang-button">
-    <a class="pp-lang-link"><span>Language</span></a>
+    <a class="pp-lang-link"><span>Language XXX</span></a>
     <div class="pp-lang-menu">
-        <a></a>
-        <a></a>
+        <a>Language 1</a>
+        <a>Language 2</a>
     </div>
 </div>
-<pre class="prettyprint">
+<pre class="prettyprint lang-xxx">
     <code></code>
 </pre>
 */
+function addLanguageSelectionMenu() {
+    // languages CSS classes
+    var languages = ['default', 'lang-html', 'lang-c', 'lang-java',
+        'lang-cs', 'lang-sh', 'lang-pl', 'lang-py', 'lang-rb',
+        'lang-js', 'lang-matlab'];
+
+    // go through each <pre> block, prepend language selection menu
+    $('pre.prettyprint').each(function () {
+        // <pre> block
+        var pre = $(this);
+
+        // current language used
+        var currLang = $.trim(this.className.replace(/prettyprint(ed)?/g, ''));
+        if (!currLang) {
+            currLang = 'default';
+        }
+
+        // create language selector button,
+        // add it to DOM just before the <pre> block,
+        // and add transparency animation on hover
+        var button = $('<div/>')
+            .addClass('pp-lang-button')
+            .attr('title', 'choose language')
+            .insertBefore(pre)
+            .hover(
+                function () {
+                    $(this).animate({opacity: 1.0}, 'fast');
+                },
+                function () {
+                    $(this).animate({opacity: 0.7});
+                }
+            );
+
+        // create toggle link, add it to button, and set click event
+        var link = $('<a/>')
+            .addClass('pp-lang-link')
+            .append($('<span/>').text(currLang))
+            .appendTo(button)
+            .click(function (ev) {
+                // set button as active/non-active, and show/hide menu
+                $(this).toggleClass('pp-link-active')
+                    .next('.pp-lang-menu').slideToggle();
+                // stop default link-clicking behaviour
+                ev.preventDefault();
+            });
+
+        // create dropmenu and add to button
+        var menu = $('<div/>')
+            .addClass('pp-lang-menu')
+            .appendTo(button);
+
+        // populate menu with languages
+        $.map(languages, function (lang) {
+            // create link, add it to menu, and set click event
+            $('<a/>')
+                .text(lang)
+                .attr('title', 'set language to: ' + lang)
+                .appendTo(menu)
+                .click(function (ev) {
+                    // reset content to plain text
+                    unprettify(pre.children('code'));
+
+                    // apply prettyprint class, and set new language
+                    pre.removeClass().addClass('prettyprint ' + lang);
+
+                    // change language indicated in selector
+                    link.children('span').text(lang);
+
+                    // hide languge menu
+                    //menu.slideToggle();
+
+                    // reapply highlighting (calls PR.prettyPrint)
+                    StackExchange.prettify.applyCodeStyling();
+
+                    // stop default link-clicking behaviour
+                    ev.preventDefault();
+                });
+        });
+    });
+}
